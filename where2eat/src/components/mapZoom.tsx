411ffe094c5820af3442055d5bfaf7a84addd622
Alexis <alexis@MacBook-Pro-de-Alexis.local>
2021-11-30 22:38:00 +0100

    remove no-used function

--- where2eat/src/components/mapZoom.tsx
+++ where2eat/src/components/mapZoom.tsx
@@ -1,14 +1,7 @@
-import React from 'react';
 import './mapZoom.css';
 
 
 export default function MapZoom(props: {zoom: number, onZoomIn: Function, onZoomOut: Function}) {
-    function handleOnChange(ev: React.ChangeEvent) {
-        // pass the values to the parent component
-        //@ts-ignore
-        props.onChange(ev.target.name, ev.target.value);
-    }
-
     function handleZoomIn(){
         props.onZoomIn()
     }
